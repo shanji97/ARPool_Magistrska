141e3d0c7c08bda074493e20d46f305921ed857f
shanji97 <aleksander.kovac97@hotmail.com>
2025-03-25 16:54:44 +0100

    #7 The basic menu now functions perfectly. App also builds and runs on MQ3.

---
+++ UnityProject/PTS_2/Assets/Scripts/PanelFunctions.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PanelFunctions : MonoBehaviour
+{
+    [Tooltip("Manually prioritized UI elements that should appear first.")]
+    public List<Transform> uiElements;
+    public Transform canvasElement;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        OrderPanelItems();
+    }
+
+
+    void Start()
+    {
+       
+       
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OrderPanelItems(){
+
+         // Get all children that are not in the uiElements list.
+         List<Transform> allChildren = new();
+        foreach(var child in allChildren){
+            if(!uiElements.Contains(child)){
+                allChildren.Add(child);
+            }
+        }
+
+        List<Transform> finalOrder = new();
+        finalOrder.AddRange(uiElements);
+        finalOrder.AddRange(allChildren);
+
+        for(var i = 0; i < finalOrder.Count; i++){
+            finalOrder[i].SetSiblingIndex(i);
+        }
+
+    }
+}
